6a912627e2433c1dfe473bcd7cdf0ed899cc133c
Arian <arian.kulmer@web.de>
2020-06-22 13:28:15 +0200

    sweet: Adapt to media HAL location
    Change-Id: Ie7d1999454acf24b5b513dd8be0763cae06e154e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -74,7 +74,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     vendor/lineage/config/device_framework_matrix.xml
 
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
-DEVICE_MANIFEST_FILE += hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_manifest.xml
+DEVICE_MANIFEST_FILE += hardware/qcom/media/conf_files/sm6150/c2_manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 ODM_MANIFEST_SKUS += sweet
 ODM_MANIFEST_SWEET_FILES := \
