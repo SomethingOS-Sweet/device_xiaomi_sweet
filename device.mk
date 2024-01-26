e573a298db79b5f581fcf4654724e7e6fef59427
basamaryan <basam.aryan@gmail.com>
2024-01-26 16:58:23 -0500

    sweet: Use common vibrator component
    Change-Id: I6217bc1245d272f85cbfa1a7d5847bea37fee4c1

--- device.mk
+++ device.mk
@@ -322,6 +322,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
     display \
     perf \
     telephony \
+    vibrator \
     wfd
 
 PRODUCT_COPY_FILES += \
@@ -370,13 +371,6 @@ PRODUCT_COPY_FILES += \
 # Vendor service manager
 PRODUCT_PACKAGES += \
     vndservicemanager
-
-# Vibrator
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # Vulkan
 PRODUCT_PACKAGES += \
