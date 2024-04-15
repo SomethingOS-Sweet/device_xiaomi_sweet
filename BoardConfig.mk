560242df5cddbe934bf5ea8bcdd7b1be1e4c6e41
anupam41 <anupamsamanta41@gmail.com>
2024-04-15 08:43:25 +0000

    sweet: Add Build broken rules

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,9 @@ BOARD_VENDOR := xiaomi
 
 DEVICE_PATH := device/xiaomi/sweet
 
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
 # Inherit from proprietary files
 include vendor/xiaomi/sweet/BoardConfigVendor.mk
 
